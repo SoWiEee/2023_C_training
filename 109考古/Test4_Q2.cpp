0dc2cc38cb1171e6c5c1df771f21934885697381
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2024-01-08 11:15:42 +0800

    Update Test4_Q2.cpp

--- 109考古/Test4_Q2.cpp
+++ 109考古/Test4_Q2.cpp
@@ -1,65 +1,47 @@
-#define _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
-#include<io.h>
 
-double sumN(int n);
-
-int main()
-{
-	FILE *binaryOutput, *binaryInput;		/* file pointers for input and backup files */
-	int integers[4] = {3, 6, 10, 11};		/* numbers will output						*/
-	int number;								/* read a integer from file					*/
-	int index;								/* loop index								*/
-	int sum;								/* sumation from 1 to any integer			*/
-	int step;								/* printing controller						*/
-	
-	/* open output file																	*/
-	binaryOutput = fopen("input.bin", "wb");
-	/* write 4 integers to output file													*/
-	for (index = 0; index < 4; index++)
-	{
-		/* write an integer to file														*/
-		fwrite(&integers[index], sizeof(int), 4, binaryOutput);
+// calculate sum from 1 to n
+double sigma(int n) {
+	if (n == 1) {
+		return 1;
 	}
-	/* close output file																*/
-	fclose(binaryOutput);
+	else if (n > 1) {
+		return n + sigma(n - 1);
+	}
+}
 
-	/* open input file																	*/		
-	binaryInput = fopen("input.bin", "rb");
-	/* read an integer from input file, calculate sumatiom from 1 to it and print		*/
-	for (index = 0; index < 4; index++)
-	{
-		/* read an integer from input file												*/
-		fread(&number, sizeof(int), 1, binaryInput);
-		/* calculate sumaiton															*/
-		sum = sumN(number);
-		/* print controller																*/
+int main(void){
+	FILE* binOut, * binIn;
+	int arr[4] = {3, 6, 10, 11};
+	int num;
+	int sum;
+	int step;	// printing controller
+
+	binOut = fopen("input.bin", "wb");
+	for (int i = 0; i < 4; i++){
+		fwrite(&arr[i], sizeof(int), 4, binOut);	// write int to file
+	}
+
+	fclose(binOut);
+
+	binIn = fopen("input.bin", "rb");
+	for (int i = 0; i < 4; i++){
+		fread(&num, sizeof(int), 1, binIn);	// read int from file
+		sum = sigma(num);
+
 		step = 1;
 		printf("%d", step);
-		/* print 1+2+...+number															*/
-		while (step < number)
-		{
-			printf("+%d", step+1);
+		// print 1+2+...+number
+		while(step < num){
+			printf("+%d", step + 1);
 			step++;
 		}
 		printf("=%d\n", sum);
 	}
-	/* close input file																	*/	
-	fclose(binaryInput);
 
+	fclose(binIn);
 	system("pause");
 	return 0;
-}
-
-double sumN(int n)	/* input - calculate sumation from 1 to n							*/
-{
-	if (n == 1)
-	{
-		return 1;				/* simple case											*/
-	}
-	else if (n > 1)
-	{
-		return n + sumN(n - 1);	/* recursive step										*/
-	}
 }
